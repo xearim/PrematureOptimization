c0d5abeda6d03e4adcd859d22cb5afcb0438557d
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-09-15 23:25:00 -0400

    Cause grade and test scripts to try mktemp if tempfile is not found.

--- tests/scanner/test.sh
+++ tests/scanner/test.sh
@@ -10,7 +10,11 @@ runscanner() {
 fail=0
 
 for file in `dirname $0`/input/*; do
-  output=`tempfile`
+  if command -v tempfile >/dev/null 2>&1; then
+      output=`tempfile`
+  else
+      output=`mktemp -t tmp`
+  fi
   runscanner $file > $output 2>&1;
   if ! diff -u $output `dirname $0`/output/`basename $file`.out; then
     echo "File $file scanner output mismatch.";
