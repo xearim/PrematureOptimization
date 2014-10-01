221ad81350f90a84e8151e734344a3573448a0ea
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-10-01 00:27:10 -0400

    Implement all AST conversions for nodes outside method declarations.

--- src/edu/mit/compilers/ast/NodeMaker.java
+++ src/edu/mit/compilers/ast/NodeMaker.java
@@ -8,6 +8,8 @@ import antlr.collections.AST;
 
 import com.google.common.collect.ImmutableList;
 
+import edu.mit.compilers.grammar.DecafParserTokenTypes;
+
 /** Utility class for dealing with ANTLR's ASTs. */
 public class NodeMaker {
     // Prohibit instantiation.
@@ -27,18 +29,83 @@ public class NodeMaker {
 
     /** Make some Callouts from a "callouts" ANTLR AST. */
     public static List<Callout> callouts(AST callouts) {
-        // TODO(jasonpr): Implement.
-        return null;
+        checkType(callouts, DecafParserTokenTypes.CALLOUTS);
+        ImmutableList.Builder<Callout> builder = ImmutableList.builder();
+        for (AST child : children(callouts)) {
+            checkType(child, DecafParserTokenTypes.ID);
+            builder.add(new Callout(child.getText()));
+        }
+        return builder.build();
     }
 
     /** Make some FieldDescriptors from a "field_decls" ANTLR AST. */
-    public static List<FieldDescriptor> fieldDescriptors(AST field_decls) {
-        // TODO(jasonpr): Implement.
-        return null;
+    public static List<FieldDescriptor> fieldDescriptors(AST fieldDecls) {
+        checkType(fieldDecls, DecafParserTokenTypes.FIELD_DECLS);
+        ImmutableList.Builder<FieldDescriptor> builder = ImmutableList.builder();
+        for (AST child : children(fieldDecls)) {
+            builder.add(fieldDescriptor(child));
+        }
+        return builder.build();
     }
 
     /** Make some Methods from a "method_decls" ANTLR AST. */
-    public static List<Method> methods(AST method_decls) {
+    public static List<Method> methods(AST methodDecls) {
+        checkType(methodDecls, DecafParserTokenTypes.METHOD_DECLS);
+        ImmutableList.Builder<Method> builder = ImmutableList.builder();
+        for (AST child : children(methodDecls)) {
+            builder.add(method(child));
+        }
+        return builder.build();
+    }
+
+    /** Make a FieldDescriptor from a "field_decl" ANTLR AST. */
+    public static FieldDescriptor fieldDescriptor(AST fieldDecl) {
+        BaseType type = baseType(fieldDecl);
+        int line = fieldDecl.getLine();
+        int column = fieldDecl.getColumn();
+
+        checkChildCount(1, fieldDecl);
+        AST namedField = fieldDecl.getFirstChild();
+        if (namedField.getType() == DecafParserTokenTypes.ID) {
+            String name = namedField.getText();
+            return new FieldDescriptor(name, line, column, type);
+        } else if (namedField.getType() == DecafParserTokenTypes.ARRAY_FIELD_DECL) {
+            checkChildCount(2, namedField);
+            List<AST> arrayInfo = children(namedField);
+            String name = stringFromId(arrayInfo.get(0));
+            // TODO(jasonpr): Decide whether we should store this as a String in
+            // FieldDescriptor.
+            int length = intFromLiteral(arrayInfo.get(1));
+            return new FieldDescriptor(name, length, line, column, type);
+        } else {
+            throw new AssertionError("AST " + fieldDecl + " is not an ID or an ARRAY_FIELD_DECL.");
+        }
+    }
+
+    public static String stringFromId(AST id) {
+        checkChildCount(0, id);
+        checkType(id, DecafParserTokenTypes.ID);
+        return id.getText();
+    }
+
+    public static int intFromLiteral(AST intLiteral) {
+        checkChildCount(0, intLiteral);
+        checkType(intLiteral, DecafParserTokenTypes.INT_LITERAL);
+        // TODO(jasonpr): Do a range check!
+        return Integer.parseInt(intLiteral.getText());
+    }
+
+    public static BaseType baseType(AST typeNode) {
+        if (typeNode.getType() == DecafParserTokenTypes.INT) {
+            return BaseType.INTEGER;
+        } else if (typeNode.getType() == DecafParserTokenTypes.BOOLEAN) {
+            return BaseType.BOOLEAN;
+        } else {
+            throw new AssertionError("Unexpected type node " + typeNode);
+        }
+    }
+
+    public static Method method(AST method) {
         // TODO(jasonpr): Implement.
         return null;
     }
@@ -50,6 +117,15 @@ public class NodeMaker {
 
     private static void checkChildCount(int expected, AST astNode) {
         checkArgument(expected == astNode.getNumberOfChildren());
+    }
+
+    private static void checkType(AST astNode, int... expected) {
+        for (int type : expected) {
+            if (type == astNode.getType()) {
+                return;
+            }
+        }
+        throw new AssertionError("AST " + astNode + " does not have a type in " + expected);
     }
 
     private static List<AST> children(AST astNode) {
