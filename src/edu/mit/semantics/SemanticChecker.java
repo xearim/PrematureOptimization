afd82dbb3a5cd1d1f01607dd8ad2c84f7551579a
Manuel Cabral <cabral@mit.edu>
2014-10-02 17:46:24 -0400

    Missing main function semantic check

--- src/edu/mit/semantics/SemanticChecker.java
+++ src/edu/mit/semantics/SemanticChecker.java
@@ -1,21 +1,9 @@
 package edu.mit.semantics;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.UnmodifiableIterator;
-
-import edu.mit.compilers.ast.Callout;
-import edu.mit.compilers.ast.FieldDescriptor;
-import edu.mit.compilers.ast.Method;
-import edu.mit.compilers.ast.Node;
-import edu.mit.compilers.ast.NodeSequence;
 import edu.mit.compilers.ast.Program;
-import edu.mit.compilers.ast.Scope;
-import edu.mit.semantics.errors.DeclaredTwiceSemanticError;
 import edu.mit.semantics.errors.SemanticError;
 
 public class SemanticChecker {
@@ -30,7 +18,8 @@ public class SemanticChecker {
 		/*
 		 * Implement checks
 		 */
-		errors.addAll(new DeclaredTwiceSemanticCheck(this.prog).doCheck());
+		errors.addAll(new DeclaredTwiceSemanticCheck(this.prog).doCheck()); // 1
+		errors.addAll(new MissingMainSemanticCheck(this.prog).doCheck()); // 3
 		
 		return errors;
 	}
---
+++ src/edu/mit/semantics/errors/MissingMainSemanticError.java
@@ -0,0 +1,12 @@
+package edu.mit.semantics.errors;
+
+
+public class MissingMainSemanticError implements SemanticError {
+
+    @Override
+    public String generateErrorMessage() {
+        // TODO Auto-generated method stub
+        return "Missing main method";
+    }
+
+}
