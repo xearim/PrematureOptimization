1b0df065993cc609b20a8cee62c34e92f10e57f0
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-10-09 13:34:51 -0400

    Remove extra code that re-implements stock Enum behavior.

--- src/edu/mit/compilers/codegen/asm/instructions/JumpType.java
+++ src/edu/mit/compilers/codegen/asm/instructions/JumpType.java
@@ -4,24 +4,14 @@ package edu.mit.compilers.codegen.asm.instructions;
  * Enumeration of jumps necessary for conditionals
  */
 public enum JumpType {
-    JZ ("JZ"), // zero
-    JNZ ("JNZ"), // not zero
-    JS ("JS"), // negative
-    JNS ("JNS"), // not negative
-    JO ("JO"), // arithmetic overflow
-    JNO ("JNO"), // not overflow
-    JL ("JL"), // signed less than
-    JLE ("JLE"), // signed less than or equal
-    JGE ("JGE"), // signed greater than or equal
-    JG ("JG"); // signed greater than
-
-    private String jumpName;
-
-    private JumpType(String jumpName) {
-        this.jumpName = jumpName;
-    }
-
-    public String toString() {
-        return this.jumpName;
-    }
+    JZ, // zero
+    JNZ, // not zero
+    JS, // negative
+    JNS, // not negative
+    JO, // arithmetic overflow
+    JNO, // not overflow
+    JL, // signed less than
+    JLE, // signed less than or equal
+    JGE, // signed greater than or equal
+    JG; // signed greater than
 }
