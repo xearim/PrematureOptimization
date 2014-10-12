0f006ee550683fd9011bc89af31900cef5ef013a
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-10-12 15:28:34 -0400

    Add a way to get a reference to a Register.
    This will allow us to hook BinOpGraphFactory up to the Instructions API.

--- src/edu/mit/compilers/codegen/asm/Register.java
+++ src/edu/mit/compilers/codegen/asm/Register.java
@@ -1,9 +1,21 @@
 package edu.mit.compilers.codegen.asm;
 
 public class Register implements Value {
-    public static enum RegisterId {
+
+    public static final Register R10 = new Register(RegisterId.R10);
+    public static final Register R11 = new Register(RegisterId.R11);
+
+    private final RegisterId id;
+
+    private static enum RegisterId {
         // TODO(manny): Add remaining registers.
         RAX, RBX, RCX, RDX, R10, R11;
+    }
+
+    // Private constructor: The only way to get a reference to a Register
+    // is through the public static instances.
+    private Register(RegisterId id) {
+        this.id = id;
     }
 
     @Override
@@ -11,6 +23,4 @@ public class Register implements Value {
         // TODO(manny): Implement.
         throw new RuntimeException("Not yet implemented.");
     }
-    
-    
 }
