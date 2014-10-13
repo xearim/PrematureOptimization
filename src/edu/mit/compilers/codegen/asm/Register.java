0992cdbf21f7be498007440aa65d45a5999bc04b
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-10-13 15:32:09 -0400

    Add more Registers.

--- src/edu/mit/compilers/codegen/asm/Register.java
+++ src/edu/mit/compilers/codegen/asm/Register.java
@@ -2,6 +2,12 @@ package edu.mit.compilers.codegen.asm;
 
 public class Register implements Value {
 
+    public static final Register RDI = new Register(RegisterId.RDI);
+    public static final Register RSI = new Register(RegisterId.RSI);
+    public static final Register RDX = new Register(RegisterId.RDX);
+    public static final Register RCX = new Register(RegisterId.RCX);
+    public static final Register R8 = new Register(RegisterId.R8);
+    public static final Register R9 = new Register(RegisterId.R9);
     public static final Register R10 = new Register(RegisterId.R10);
     public static final Register R11 = new Register(RegisterId.R11);
 
@@ -9,7 +15,7 @@ public class Register implements Value {
 
     private static enum RegisterId {
         // TODO(manny): Add remaining registers.
-        RAX, RBX, RCX, RDX, R10, R11;
+        RAX, RBX, RCX, RDX, RDI, RSI, R8, R9, R10, R11;
     }
 
     // Private constructor: The only way to get a reference to a Register
