95f861ccb79cf2ab116edc54d6658639bf95b885
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-10-13 14:54:49 -0400

    Implement TernaryOpGraphFactory.

---
+++ src/edu/mit/compilers/codegen/controllinker/TernaryOpGraphFactory.java
@@ -0,0 +1,26 @@
+package edu.mit.compilers.codegen.controllinker;
+
+import edu.mit.compilers.ast.Scope;
+import edu.mit.compilers.ast.TernaryOperation;
+
+public class TernaryOpGraphFactory implements GraphFactory {
+
+    private final TerminaledGraph graph;
+    
+    public TernaryOpGraphFactory(TernaryOperation operation, Scope scope) {
+        this.graph = calculateGraph(operation, scope);
+    }
+
+    private TerminaledGraph calculateGraph(TernaryOperation operation, Scope scope) {
+        return new BranchGraphFactory(
+                new NativeExprGraphFactory(operation.getCondition(), scope).getGraph(),
+                new NativeExprGraphFactory(operation.getCondition(), scope).getGraph(),
+                new NativeExprGraphFactory(operation.getCondition(), scope).getGraph())
+                .getGraph();
+    }
+
+    @Override
+    public TerminaledGraph getGraph() {
+        return graph;
+    }
+}
