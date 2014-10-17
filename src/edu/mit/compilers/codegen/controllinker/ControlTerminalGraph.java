395a0ae11568b75124d2ce85fd90aca300b54e7f
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-10-17 16:46:20 -0400

    Add ControlTerminalGraph.
    ControlTreminalGraph allows the control flows of breaks, continues,
    and returns to be handled easily.

---
+++ src/edu/mit/compilers/codegen/controllinker/ControlTerminalGraph.java
@@ -0,0 +1,64 @@
+package edu.mit.compilers.codegen.controllinker;
+
+import edu.mit.compilers.codegen.ControlFlowNode;
+import edu.mit.compilers.codegen.SequentialControlFlowNode;
+
+
+/**
+ * A control flow graph with a a beginning node and and end node, and three special
+ * control flow nodes.
+ *
+ * <p>Like BiTerminalGraph, this graph has one beginning node and one end node.  Unlike
+ * BiTerminalGraph, it has three alternate end nodes: a "break", a "continue", and a
+ * "return" node.
+ *
+ */
+public class ControlTerminalGraph {
+    private final ControlFlowNode beginning;
+    private final SequentialControlFlowNode end;
+    private final ControlNodes controlNodes;
+
+    public static class ControlNodes {
+        private final SequentialControlFlowNode breakNode;
+        private final SequentialControlFlowNode continueNode;
+        private final SequentialControlFlowNode returnNode;
+
+        public ControlNodes(SequentialControlFlowNode breakNode,
+                SequentialControlFlowNode continueNode, SequentialControlFlowNode returnNode) {
+            this.breakNode = breakNode;
+            this.continueNode = continueNode;
+            this.returnNode = returnNode;
+        }
+
+        public SequentialControlFlowNode getBreakNode() {
+            return breakNode;
+        }
+
+        public SequentialControlFlowNode getContinueNode() {
+            return continueNode;
+        }
+
+        public SequentialControlFlowNode getReturnNode() {
+            return returnNode;
+        }
+    }
+
+    public ControlTerminalGraph(
+            ControlFlowNode beginning, SequentialControlFlowNode end,ControlNodes controlNodes) {
+        this.beginning = beginning;
+        this.end = end;
+        this.controlNodes = controlNodes;
+    }
+
+    public ControlFlowNode getBeginning() {
+        return beginning;
+    }
+
+    public SequentialControlFlowNode getEnd() {
+        return end;
+    }
+
+    public ControlNodes getControlNodes() {
+        return controlNodes;
+    }
+}
