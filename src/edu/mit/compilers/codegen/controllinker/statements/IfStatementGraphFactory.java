4fa1cb5b1a5ec2d973a69fa3e64e24030ff91b4e
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-10-19 17:29:55 -0400

    Remove Labels from IfStatementGraphFactory.

--- src/edu/mit/compilers/codegen/controllinker/statements/IfStatementGraphFactory.java
+++ src/edu/mit/compilers/codegen/controllinker/statements/IfStatementGraphFactory.java
@@ -1,20 +1,18 @@
 package edu.mit.compilers.codegen.controllinker.statements;
 
-import com.google.common.base.Optional;
-
 import static edu.mit.compilers.codegen.asm.instructions.Instructions.compareFlagged;
 import static edu.mit.compilers.codegen.asm.instructions.Instructions.move;
 import static edu.mit.compilers.codegen.asm.instructions.Instructions.pop;
-import static edu.mit.compilers.codegen.asm.instructions.Instructions.writeLabel;
+
+import com.google.common.base.Optional;
+
 import edu.mit.compilers.ast.Block;
 import edu.mit.compilers.ast.IfStatement;
 import edu.mit.compilers.ast.Scope;
 import edu.mit.compilers.codegen.BranchingControlFlowNode;
 import edu.mit.compilers.codegen.SequentialControlFlowNode;
-import edu.mit.compilers.codegen.asm.Label;
 import edu.mit.compilers.codegen.asm.Literal;
 import edu.mit.compilers.codegen.asm.Register;
-import edu.mit.compilers.codegen.asm.Label.LabelType;
 import edu.mit.compilers.codegen.asm.instructions.JumpType;
 import edu.mit.compilers.codegen.controllinker.BiTerminalGraph;
 import edu.mit.compilers.codegen.controllinker.BlockGraphFactory;
@@ -37,19 +35,7 @@ public class IfStatementGraphFactory implements ControlTerminalGraphFactory {
         SequentialControlFlowNode continueNode = SequentialControlFlowNode.nopTerminal();
         SequentialControlFlowNode breakNode = SequentialControlFlowNode.nopTerminal();
         SequentialControlFlowNode returnNode = SequentialControlFlowNode.nopTerminal(); 
-        
-        // Create the two labels we need to have a for loop
-        // One label for the else block
-        SequentialControlFlowNode elseLabel = SequentialControlFlowNode.terminal(writeLabel(
-        											new Label(LabelType.CONTROL_FLOW, "elseif" + ifStatement.getID())));
-        // A second label for the end of the if statement
-        SequentialControlFlowNode endLabel = SequentialControlFlowNode.terminal(writeLabel(
-													new Label(LabelType.CONTROL_FLOW, "endif" + ifStatement.getID())));
-        
-        // And link the end to its label
-        endLabel.setNext(end);
-        BiTerminalGraph endTarget = new BiTerminalGraph(endLabel, end);
-        
+
         // Conditional
         BiTerminalGraph conditionalGraph = new NativeExprGraphFactory(
                 ifStatement.getCondition(), scope).getGraph();
@@ -68,7 +54,7 @@ public class IfStatementGraphFactory implements ControlTerminalGraphFactory {
         thenBlockGraph.getControlNodes().getBreakNode().setNext(breakNode);
         thenBlockGraph.getControlNodes().getContinueNode().setNext(continueNode);
         thenBlockGraph.getControlNodes().getReturnNode().setNext(returnNode);
-        thenBlockGraph.getEnd().setNext(endTarget.getBeginning());
+        thenBlockGraph.getEnd().setNext(end);
 
         // Obtain else block
         Optional<Block> elseBlock = ifStatement.getElseBlock();
@@ -78,17 +64,13 @@ public class IfStatementGraphFactory implements ControlTerminalGraphFactory {
         elseBlockGraph.getControlNodes().getBreakNode().setNext(breakNode);
         elseBlockGraph.getControlNodes().getContinueNode().setNext(continueNode);
         elseBlockGraph.getControlNodes().getReturnNode().setNext(returnNode);
-        elseBlockGraph.getEnd().setNext(endTarget.getBeginning());
-        
-        // And link the else block to its label
-        elseLabel.setNext(elseBlockGraph.getBeginning());
-        BiTerminalGraph elseTarget = new BiTerminalGraph(elseLabel, elseBlockGraph.getEnd());
-        
+        elseBlockGraph.getEnd().setNext(end);
+
         BranchingControlFlowNode branch =
                 new BranchingControlFlowNode(
                         JumpType.JNE,
                         thenBlockGraph.getBeginning(),
-                        elseTarget.getBeginning());
+                        elseBlockGraph.getBeginning());
         ifComparator.getEnd().setNext(branch);
         
         return new ControlTerminalGraph(start, end,
