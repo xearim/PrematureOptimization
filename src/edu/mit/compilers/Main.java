da39813f6fb267edc72a82926696613959b15889
Manuel Cabral <cabral@mit.edu>
2014-10-18 21:51:04 -0400

    Added codegen function to main
    Fixed whitespace

--- src/edu/mit/compilers/Main.java
+++ src/edu/mit/compilers/Main.java
@@ -34,100 +34,100 @@ import edu.mit.semantics.errors.SemanticError;
 
 class Main {
 
-  private static final String MAIN_METHOD_NAME = "main";
+    private static final String MAIN_METHOD_NAME = "main";
 
-  public static void main(String[] args) {
-    try {
-      // Setup in and out files.
-      CLI.parse(args, new String[0]);
-      InputStream inputStream = args.length == 0 ?
-          System.in : new java.io.FileInputStream(CLI.infile);
-      PrintStream outputStream = CLI.outfile == null ? System.out : new java.io.PrintStream(new java.io.FileOutputStream(CLI.outfile));
+    public static void main(String[] args) {
+        try {
+            // Setup in and out files.
+            CLI.parse(args, new String[0]);
+            InputStream inputStream = args.length == 0 ?
+                    System.in : new java.io.FileInputStream(CLI.infile);
+            PrintStream outputStream = CLI.outfile == null ? System.out : new java.io.PrintStream(new java.io.FileOutputStream(CLI.outfile));
 
-      // Parse or scan.
-      if (CLI.target == Action.SCAN) {
-        scan(inputStream, outputStream);
-      } else if (CLI.target == Action.AST) {
-        printAst(inputStream, outputStream);
-      } else if (CLI.target == Action.INTER) {
-        semanticCheck(inputStream, outputStream);
-      } else if (CLI.target == Action.CFG) {
-          controlFlowGraph(inputStream, outputStream);
-      } else if (CLI.target == Action.PARSE ||
-                 CLI.target == Action.DEFAULT) {
-        parse(inputStream, outputStream);
-      }
-    } catch(Exception e) {
-      // An unrecoverable error occurred.
-      System.err.println(CLI.infile+" "+e);
-    }
-  }
-
-  // TODO(jasonpr): Javadoc.
-  private static void scan(InputStream inputStream, PrintStream outputStream) throws CharStreamException {
-    DecafScanner scanner =
-        new DecafScanner(new DataInputStream(inputStream));
-    scanner.setTrace(CLI.debug);
-    Token token;
-    boolean done = false;
-    while (!done) {
-      try {
-        for (token = scanner.nextToken();
-             token.getType() != DecafParserTokenTypes.EOF;
-             token = scanner.nextToken()) {
-          String type = "";
-          String text = token.getText();
-          switch (token.getType()) {
-           // TODO: add strings for the other types here...
-           case DecafScannerTokenTypes.ID:
-             type = " IDENTIFIER";
-             break;
-           case DecafScannerTokenTypes.CHAR:
-             type = " CHARLITERAL";
-             break;
-           case DecafScannerTokenTypes.STRING:
-             type = " STRINGLITERAL";
-             break;
-           case DecafScannerTokenTypes.TRUE:  /* fall-through */
-           case DecafScannerTokenTypes.FALSE:
-             type = " BOOLEANLITERAL";
-             break;
-           case DecafScannerTokenTypes.INT_LITERAL:
-             type = " INTLITERAL";
-             break;
-          }
-          outputStream.println(token.getLine() + type + " " + text);
+            // Parse or scan.
+            if (CLI.target == Action.SCAN) {
+                scan(inputStream, outputStream);
+            } else if (CLI.target == Action.AST) {
+                printAst(inputStream, outputStream);
+            } else if (CLI.target == Action.INTER) {
+                semanticCheck(inputStream, outputStream);
+            } else if (CLI.target == Action.CFG) {
+                controlFlowGraph(inputStream, outputStream);
+            } else if (CLI.target == Action.PARSE ||
+                    CLI.target == Action.DEFAULT) {
+                parse(inputStream, outputStream);
+            } else if (CLI.target == Action.ASSEMBLY) {
+                genCode(inputStream, outputStream);
+            }
+        } catch(Exception e) {
+            // An unrecoverable error occurred.
+            System.err.println(CLI.infile+" "+e);
         }
-        done = true;
-      } catch(Exception e) {
-        // print the error:
-        System.err.println(CLI.infile + " " + e);
-        scanner.consume();
-      }
     }
-  }
 
-  // TODO(jasonpr): Javadoc.
-  private static void parse(InputStream inputStream, PrintStream outputStream) throws RecognitionException, TokenStreamException {
-    DecafParser parser = programmedParser(inputStream, outputStream);
-    if(parser.getError()) {
-      System.exit(1);
+    // TODO(jasonpr): Javadoc.
+    private static void scan(InputStream inputStream, PrintStream outputStream) throws CharStreamException {
+        DecafScanner scanner =
+                new DecafScanner(new DataInputStream(inputStream));
+        scanner.setTrace(CLI.debug);
+        Token token;
+        boolean done = false;
+        while (!done) {
+            try {
+                for (token = scanner.nextToken();
+                        token.getType() != DecafParserTokenTypes.EOF;
+                        token = scanner.nextToken()) {
+                    String type = "";
+                    String text = token.getText();
+                    switch (token.getType()) {
+                        // TODO: add strings for the other types here...
+                        case DecafScannerTokenTypes.ID:
+                            type = " IDENTIFIER";
+                            break;
+                        case DecafScannerTokenTypes.CHAR:
+                            type = " CHARLITERAL";
+                            break;
+                        case DecafScannerTokenTypes.STRING:
+                            type = " STRINGLITERAL";
+                            break;
+                        case DecafScannerTokenTypes.TRUE:  /* fall-through */
+                        case DecafScannerTokenTypes.FALSE:
+                            type = " BOOLEANLITERAL";
+                            break;
+                        case DecafScannerTokenTypes.INT_LITERAL:
+                            type = " INTLITERAL";
+                            break;
+                    }
+                    outputStream.println(token.getLine() + type + " " + text);
+                }
+                done = true;
+            } catch(Exception e) {
+                // print the error:
+                System.err.println(CLI.infile + " " + e);
+                scanner.consume();
+            }
+        }
     }
-  }
 
-  /** Print the AST that ANTLR generated for the program, in DOT format. */
-  private static void printAst(InputStream inputStream, PrintStream outputStream)
-          throws RecognitionException, TokenStreamException {
-      DecafParser parser = programmedParser(inputStream, outputStream);
-      if (parser.getError()) {
-          System.exit(1);
-      }
-      AST ast = parser.getAST();
-      AstPrinter printer = new AstPrinter(outputStream);
-      printer.print(ast);
-  }
+    // TODO(jasonpr): Javadoc.
+    private static void parse(InputStream inputStream, PrintStream outputStream) throws RecognitionException, TokenStreamException {
+        DecafParser parser = programmedParser(inputStream, outputStream);
+        if(parser.getError()) {
+            System.exit(1);
+        }
+    }
 
-    // TODO(jasonpr): Fix all the indentation in this file!
+    /** Print the AST that ANTLR generated for the program, in DOT format. */
+    private static void printAst(InputStream inputStream, PrintStream outputStream)
+            throws RecognitionException, TokenStreamException {
+        DecafParser parser = programmedParser(inputStream, outputStream);
+        if (parser.getError()) {
+            System.exit(1);
+        }
+        AST ast = parser.getAST();
+        AstPrinter printer = new AstPrinter(outputStream);
+        printer.print(ast);
+    }
 
     private static void semanticCheck(InputStream inputStream, PrintStream outputStream)
             throws RecognitionException, TokenStreamException {
@@ -173,44 +173,49 @@ class Main {
     }
 
 
-  /**
-   * Makes a DecafParser, use it to parse the program from inputStream, and return that it.
-   *
-   * <p>The returned parser might be in an error state.  This function does not react to any
-   * parser errors.
-   */
-  private static DecafParser programmedParser(InputStream inputStream, PrintStream outputStream)
-          throws RecognitionException, TokenStreamException {
-      DecafScanner scanner =
-              new DecafScanner(new DataInputStream(inputStream));
-          DecafParser parser = new DecafParser(scanner);
-          // Need a custom ASTFactory with replaced nodes to actually get line info
-          ASTFactory factory = new ASTFactory();                         
-          factory.setASTNodeClass(AntlrASTWithLines.class);
-          parser.setASTFactory(factory);
-          parser.setTrace(CLI.debug);
-          parser.program();
-          return parser;
-  }
+    /**
+     * Makes a DecafParser, use it to parse the program from inputStream, and return that it.
+     *
+     * <p>The returned parser might be in an error state.  This function does not react to any
+     * parser errors.
+     */
+    private static DecafParser programmedParser(InputStream inputStream, PrintStream outputStream)
+            throws RecognitionException, TokenStreamException {
+        DecafScanner scanner =
+                new DecafScanner(new DataInputStream(inputStream));
+        DecafParser parser = new DecafParser(scanner);
+        // Need a custom ASTFactory with replaced nodes to actually get line info
+        ASTFactory factory = new ASTFactory();
+        factory.setASTNodeClass(AntlrASTWithLines.class);
+        parser.setASTFactory(factory);
+        parser.setTrace(CLI.debug);
+        parser.program();
+        return parser;
+    }
 
-  /**
-   * Gets the semantically valid Program AST Node for the program on the inputStream.
-   *
-   * <p>Returns Optional.absent() for an invalid program.
-   */
-  private static Optional<Program>
-          semanticallyValidProgram(InputStream inputStream, PrintStream outputStream)
-          throws RecognitionException, TokenStreamException {
-      DecafParser parser = programmedParser(inputStream, outputStream);
-      if (parser.getError()) {
-          // It didn't even parse!
-          return Optional.absent();
-      }
-      AST ast = parser.getAST();
-      Program program = NodeMaker.program(ast);
+    /**
+     * Gets the semantically valid Program AST Node for the program on the inputStream.
+     *
+     * <p>Returns Optional.absent() for an invalid program.
+     */
+    private static Optional<Program>
+    semanticallyValidProgram(InputStream inputStream, PrintStream outputStream)
+            throws RecognitionException, TokenStreamException {
+        DecafParser parser = programmedParser(inputStream, outputStream);
+        if (parser.getError()) {
+            // It didn't even parse!
+            return Optional.absent();
+        }
+        AST ast = parser.getAST();
+        Program program = NodeMaker.program(ast);
 
-      return isSemanticallyValid(program, outputStream)
-              ? Optional.of(program)
-              : Optional.<Program>absent();
-      }
+        return isSemanticallyValid(program, outputStream)
+                ? Optional.of(program)
+                        : Optional.<Program>absent();
+    }
+
+    private static void genCode(InputStream inputStream, PrintStream outputStream) {
+
+
+    }
 }
