bc5f67be3b5e5aa9e6f58c21e141f16967379bc5
David Stingley <xearim@gmail.com>
2014-10-18 20:46:53 -0400

    fix up whileLoopGraphFactory

--- src/edu/mit/compilers/codegen/controllinker/statements/WhileLoopGraphFactory.java
+++ src/edu/mit/compilers/codegen/controllinker/statements/WhileLoopGraphFactory.java
@@ -22,8 +22,8 @@ import edu.mit.compilers.codegen.controllinker.NativeExprGraphFactory;
 public class WhileLoopGraphFactory implements ControlTerminalGraphFactory {
     private final ControlTerminalGraph graph;
 
-    public WhileLoopGraphFactory(WhileLoop wl, Scope scope) {
-        this.graph = calculateGraph(wl,scope);
+    public WhileLoopGraphFactory(WhileLoop whileLoop, Scope scope) {
+        this.graph = calculateGraph(whileLoop,scope);
     }
 
     private ControlTerminalGraph calculateGraph(WhileLoop whileLoop, Scope scope) {
@@ -34,69 +34,69 @@ public class WhileLoopGraphFactory implements ControlTerminalGraphFactory {
         SequentialControlFlowNode returnNode = SequentialControlFlowNode.nopTerminal(); 
         SequentialControlFlowNode loopStart = SequentialControlFlowNode.nopTerminal();
 
+
+    	// Assign the max Rep var
+        BiTerminalGraph maxRepetitionsAssigner = BiTerminalGraph.ofInstructions(
+        											push(Literal.INITIAL_VALUE)
+        											);
+        // Remove the max Rep var
+        BiTerminalGraph maxRepetitionsDestroyer = BiTerminalGraph.ofInstructions(
+				pop(Register.R10) // pop our max rep var off the stack
+				);
+        
+        // Comparisons for the loop
+        BiTerminalGraph loopComparator = BiTerminalGraph.sequenceOf(
+                new NativeExprGraphFactory(whileLoop.getCondition(), scope).getGraph(),
+                BiTerminalGraph.ofInstructions(
+                        pop(Register.R10), // Evaluated conditional.
+                        move(Literal.TRUE, Register.R11), // true.
+                        compareFlagged(Register.R11, Register.R10)));
+
+        // increment the max Repetitions var towards maxRepetitions
+        BiTerminalGraph incrementor = BiTerminalGraph.ofInstructions(
+                pop(Register.R10),
+                increment(Register.R10),
+                push(Register.R10));
+        
+        // The actual execution body
+        ControlTerminalGraph body = new BlockGraphFactory(whileLoop.getBody()).getGraph();
+        
+        // The branch for checking repetition number
+        BranchingControlFlowNode maxRepetitionBranch = new BranchingControlFlowNode(JumpType.JL, 
+        								loopComparator.getBeginning(), maxRepetitionsDestroyer.getBeginning());
+
+        // The branch for checking conditionals
+        BranchingControlFlowNode conditionalBranch = new BranchingControlFlowNode(JumpType.JNE, 
+        									body.getBeginning(), maxRepetitionsDestroyer.getBeginning());
         
         if(whileLoop.getMaxRepetitions().isPresent()){
-        	// Assign the max Rep var
-	        BiTerminalGraph maxRepetitionsAssigner = BiTerminalGraph.ofInstructions(
-	        											push(Literal.INITIAL_VALUE)
-	        											);
+        	// When you have max repetitions, you need to actually pop off that variable before returning.
+	        returnNode = SequentialControlFlowNode.terminal(pop(Register.R10));
 	        
-	        // Comparisons for the loop
-	        BiTerminalGraph loopComparator = BiTerminalGraph.sequenceOf(
-	                new NativeExprGraphFactory(whileLoop.getCondition(), scope).getGraph(),
-	                BiTerminalGraph.ofInstructions(
-	                        pop(Register.R10), // Evaluated conditional.
-	                        move(Literal.TRUE, Register.R11), // true.
-	                        compareFlagged(Register.R11, Register.R10)));
-	
 	        // Comparisons for the max Repetitions
 	        BiTerminalGraph maxRepetitionsComparator = BiTerminalGraph.ofInstructions(
 	                        pop(Register.R10), // max repetitions counter
 	                        move(new Literal(whileLoop.getMaxRepetitions().get().get64BitValue()), Register.R11), // max repetitions
 	                        compareFlagged(Register.R11, Register.R10));
-	
-	        // increment the max Repetitions var towards maxRepetitions
-	        BiTerminalGraph incrementor = BiTerminalGraph.ofInstructions(
-	                pop(Register.R10),
-	                increment(Register.R10),
-	                push(Register.R10));
 	        
-	        // The actual execution body
-	        ControlTerminalGraph body = new BlockGraphFactory(whileLoop.getBody()).getGraph();
+	        // The branch for checking conditionals when you have max reps
+	        conditionalBranch = new BranchingControlFlowNode(JumpType.JNE, 
+	        				body.getBeginning(), maxRepetitionsDestroyer.getBeginning());
 	        
-	        // The branch for checking repetition number
-	        BranchingControlFlowNode maxRepetitionBranch = new BranchingControlFlowNode(JumpType.JL, loopStart, end);
-	
-	        // The branch for checking conditionals
-	        BranchingControlFlowNode conditionalBranch = new BranchingControlFlowNode(JumpType.JNE, body.getBeginning(), end);
-	
 	        // All the nodes have been made.  Make the connections.
 	        start.setNext(maxRepetitionsAssigner.getBeginning());
 	        maxRepetitionsAssigner.getEnd().setNext(loopStart);
-	        loopStart.setNext(loopComparator.getBeginning());
+	        loopStart.setNext(maxRepetitionsComparator.getBeginning());
+	        maxRepetitionsComparator.getEnd().setNext(maxRepetitionBranch);
 	        loopComparator.getEnd().setNext(conditionalBranch);
 	        body.getEnd().setNext(incrementor.getBeginning());
-	        incrementor.getEnd().setNext(maxRepetitionsComparator.getBeginning());
-	        maxRepetitionsComparator.getEnd().setNext(maxRepetitionBranch);
-	        body.getControlNodes().getBreakNode().setNext(end);
+	        incrementor.getEnd().setNext(loopStart);
+	        maxRepetitionsDestroyer.getEnd().setNext(end);
+	        body.getControlNodes().getBreakNode().setNext(maxRepetitionsDestroyer.getBeginning());
 	        body.getControlNodes().getContinueNode().setNext(incrementor.getBeginning());
 	        body.getControlNodes().getReturnNode().setNext(returnNode);
-        } else {	        
-	        // Comparisons for the loop
-	        BiTerminalGraph loopComparator = BiTerminalGraph.sequenceOf(
-	                new NativeExprGraphFactory(whileLoop.getCondition(), scope).getGraph(),
-	                BiTerminalGraph.ofInstructions(
-	                        pop(Register.R10), // Evaluated conditional.
-	                        move(Literal.TRUE, Register.R11), // true.
-	                        compareFlagged(Register.R11, Register.R10)));
-	        
-	        // The actual execution body
-	        ControlTerminalGraph body = new BlockGraphFactory(whileLoop.getBody()).getGraph();
-
-	        // The branch for checking conditionals
-	        BranchingControlFlowNode conditionalBranch = new BranchingControlFlowNode(JumpType.JNE, body.getBeginning(), end);
-	
-	        // All the nodes have been made.  Make the connections.
+        } else {
+        	// All the nodes have been made.  Make the connections.
 	        start.setNext(loopStart);
 	        loopStart.setNext(loopComparator.getBeginning());
 	        loopComparator.getEnd().setNext(conditionalBranch);
