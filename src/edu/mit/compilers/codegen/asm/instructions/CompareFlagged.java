2b80bdd01e46d34e733efa755d037d85929d4083
David Stingley <xearim@gmail.com>
2014-10-20 02:25:03 -0400

    fix compare to use registers for safety

--- src/edu/mit/compilers/codegen/asm/instructions/CompareFlagged.java
+++ src/edu/mit/compilers/codegen/asm/instructions/CompareFlagged.java
@@ -1,5 +1,6 @@
 package edu.mit.compilers.codegen.asm.instructions;
 
+import edu.mit.compilers.codegen.asm.Register;
 import edu.mit.compilers.codegen.asm.Value;
 
 public class CompareFlagged implements Instruction {
@@ -14,9 +15,16 @@ public class CompareFlagged implements Instruction {
 
     @Override
     public String inAttSyntax() {
-    	String syntax = "cmp ";
+    	String syntax = "";
+    	// For now we are going to be extra safe and register everything up
+    	// TODO:(xearim) fix specs or something so this isnt so dirty
+    	syntax += Instructions.push(rightArgument).inAttSyntax() + "\n";
+    	syntax += Instructions.push(leftArgument).inAttSyntax() + "\n";
+    	syntax += Instructions.pop(Register.R10).inAttSyntax() + "\n";
+    	syntax += Instructions.pop(Register.R11).inAttSyntax() + "\n";
+    	syntax += "cmp ";
     	// because of x86, gotta flip arguments to maintain abstraction
-    	syntax += rightArgument.inAttSyntax() + ", " + leftArgument.inAttSyntax();
+    	syntax += Register.R11.inAttSyntax() + ", " + Register.R10.inAttSyntax();
     	return syntax;
     }
 
