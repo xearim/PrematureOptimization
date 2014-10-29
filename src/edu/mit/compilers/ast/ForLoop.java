17b1dc32f12478023a6a4cb2e4a34310710a05b5
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-10-29 14:11:36 -0400

    Remove loop ID from ForLoop.
    It is dead code/dead data.  I don't want to need to account for it
    when I implement hashCode and equals.

--- src/edu/mit/compilers/ast/ForLoop.java
+++ src/edu/mit/compilers/ast/ForLoop.java
@@ -3,14 +3,12 @@ package edu.mit.compilers.ast;
 import com.google.common.collect.ImmutableList;
 
 public class ForLoop implements Statement {
-	private static long forLoopIDGenerator = 0;
 
     private final ScalarLocation loopVariable;
     private final NativeExpression rangeStart;
     private final NativeExpression rangeEnd;
     private final Block body;
     private final LocationDescriptor locationDescriptor;
-    private final long forLoopID;
 
     public ForLoop(ScalarLocation loopVariable, NativeExpression rangeStart,
 		   NativeExpression rangeEnd, Block body, LocationDescriptor locationDescriptor) {
@@ -19,7 +17,6 @@ public class ForLoop implements Statement {
         this.rangeEnd = rangeEnd;
         this.body = body;
         this.locationDescriptor = locationDescriptor;
-        this.forLoopID = forLoopIDGenerator++;
     }
     
     @Override
@@ -70,10 +67,4 @@ public class ForLoop implements Statement {
     public Block getBody() {
         return body;
     }
-    
-    public String getID() {
-    	return Long.toString(forLoopID);
-    }
-
-    // TODO(jasonpr): Implement equals, hashCode, and toString.
 }
