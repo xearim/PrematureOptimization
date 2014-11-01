305d45826ddea5035353d0fdf95015da91eef98b
Manuel Cabral <cabral@mit.edu>
2014-11-01 18:58:15 -0400

    Fixed Subexpression@getGeneralScope

--- src/edu/mit/compilers/optimization/Subexpression.java
+++ src/edu/mit/compilers/optimization/Subexpression.java
@@ -6,6 +6,13 @@ import edu.mit.compilers.ast.Location;
 import edu.mit.compilers.ast.NativeExpression;
 import edu.mit.compilers.ast.Scope;
 
+/**
+ * Contains the native expression and its immediate scope. It is necessary to
+ * keep track of the scope for a native expression in cases such as the
+ * following:
+ * - two if statements at the same level use the same variable name.
+ * - an expressions is derived only from variables from higher scopes.
+ */
 public class Subexpression {
     private NativeExpression ne;
     private Scope scope;
@@ -20,17 +27,26 @@ public class Subexpression {
 
     /**
      * Finds the first scope that contains a variable from the subexpression
-     * @return
      */
     public Scope getGeneralScope() {
-        Scope s = this.scope;
-        Set<Location> variables = getVariables(this.ne);
+        Set<Location> variables = getVariables();
 
-        do {
+        // Check immediate scope
+        if (containsAVariable(this.scope, variables)) {
+            return this.scope;
+        }
+
+        /*
+         * If no variable in is immediate scope, recurse through scopes until
+         * a scope that contains at least on variable is found.
+         */
+        Scope s = this.scope;
+        while (s.getParent().isPresent()) {
+            s = s.getParent().get();
             if (containsAVariable(s, variables)) {
                 return s;
             }
-        } while (s.getParent().isPresent());
+        }
         
         throw new AssertionError("Subexpression.java: Subexpression variables don't exist in Scope.");
     }
@@ -47,7 +63,7 @@ public class Subexpression {
         throw new RuntimeException("Subexpression#containsAVariableFromNativeExpression unimplemented.");
     }
 
-    private Set<Location> getVariables(NativeExpression ne) {
+    private Set<Location> getVariables() {
         throw new RuntimeException("Subexpression#getVariables unimplemented.");
     }
 }
