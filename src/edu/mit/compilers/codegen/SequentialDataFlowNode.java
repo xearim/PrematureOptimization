95eb29c8b6ea67a0307f9626861b6592b3282e99
Manuel Cabral <cabral@mit.edu>
2014-11-01 23:49:22 -0400

    Added getPredecessors and getSuccesors to DataFlowNode
    It will be useful later on to have one consistent way of getting
    predecessors and successors from a DataFlowNode.

--- src/edu/mit/compilers/codegen/SequentialDataFlowNode.java
+++ src/edu/mit/compilers/codegen/SequentialDataFlowNode.java
@@ -1,10 +1,12 @@
 package edu.mit.compilers.codegen;
 
 import java.util.Collection;
+import java.util.Set;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 public class SequentialDataFlowNode extends DataFlowNode{
 
@@ -12,48 +14,48 @@ public class SequentialDataFlowNode extends DataFlowNode{
     private final String name;
 
     private SequentialDataFlowNode(Optional<DataFlowNode> prev, Optional<DataFlowNode> next, String name) {
-    	super();
+        super();
         this.prev = prev;
         this.next = next;
         this.name = name;
     }
-    
+
     public SequentialDataFlowNode(String name) {
-    	this(Optional.<DataFlowNode>absent(), Optional.<DataFlowNode>absent(), name);
+        this(Optional.<DataFlowNode>absent(), Optional.<DataFlowNode>absent(), name);
     }
-    
+
     public static SequentialDataFlowNode nop() {
         return new SequentialDataFlowNode("");
     }
-    
+
     public static SequentialDataFlowNode nopNamed(String name) {
-    	return new SequentialDataFlowNode(name);
+        return new SequentialDataFlowNode(name);
     }
-    
+
     public static void link(SequentialDataFlowNode prev, SequentialDataFlowNode next) {
-    	prev.setNext(next);
-    	next.setPrev(prev);
+        prev.setNext(next);
+        next.setPrev(prev);
     }
-    
+
     public static void link(SequentialDataFlowNode prev, BranchSinkDataFlowNode next) {
-    	prev.setNext(next);
-    	next.setPrev(prev);
+        prev.setNext(next);
+        next.setPrev(prev);
     }
-    
+
     public static void link(SequentialDataFlowNode prev, BranchSourceDataFlowNode next) {
-    	prev.setNext(next);
-    	next.setPrev(prev);
+        prev.setNext(next);
+        next.setPrev(prev);
     }
-    
+
     public static void link(BranchSinkDataFlowNode prev, SequentialDataFlowNode next) {
-    	prev.setNext(next);
-    	next.setPrev(prev);
+        prev.setNext(next);
+        next.setPrev(prev);
     }
 
     public boolean hasNext() {
         return next.isPresent();
     }
-    
+
     public boolean hasPrev() {
         return prev.isPresent();
     }
@@ -61,25 +63,25 @@ public class SequentialDataFlowNode extends DataFlowNode{
     public DataFlowNode getNext() {
         return next.get();
     }
-    
+
     public DataFlowNode getPrev() {
         return prev.get();
     }
-    
+
     public void setNext(DataFlowNode next) {
         Preconditions.checkState(!this.equals(next));
         this.next = Optional.of(next);
     }
-    
+
     public void setPrev(DataFlowNode prev) {
         Preconditions.checkState(!this.equals(prev));
         this.prev = Optional.of(prev);
     }
-    
+
     public void clearNext() {
         this.next = Optional.absent();
     }
-    
+
     public void clearPrev() {
         this.prev = Optional.absent();
     }
@@ -88,11 +90,23 @@ public class SequentialDataFlowNode extends DataFlowNode{
     public Collection<DataFlowNode> getSinks() {
         return hasNext()
                 ? ImmutableList.<DataFlowNode>of(getNext())
-                : ImmutableList.<DataFlowNode>of();
+                        : ImmutableList.<DataFlowNode>of();
     }
 
     @Override
     public String nodeText() {
         return name;
+    }
+
+    public Set<DataFlowNode> getPredecessors() {
+        return (hasPrev())
+                ? ImmutableSet.<DataFlowNode>of(getNext())
+                        : ImmutableSet.<DataFlowNode>of();
+    }
+
+    public Set<DataFlowNode> getSuccessors() {
+        return (hasNext())
+                ? ImmutableSet.<DataFlowNode>of(getPrev())
+                : ImmutableSet.<DataFlowNode>of();
     }
 }
