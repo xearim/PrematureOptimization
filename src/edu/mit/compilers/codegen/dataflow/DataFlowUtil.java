14d21fbba6356413035fb910f8018a946f4b42cc
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-02 15:16:45 -0500

    Add DataFlowUtil, with #reachableFrom.
    Sometimes we pass around a DataFlowNode called 'head', and allow it to
    represent the entire graph that is reachable from it.  This utility
    allows us to enumerate all the DataFlowNodes in that graph.

---
+++ src/edu/mit/compilers/codegen/dataflow/DataFlowUtil.java
@@ -0,0 +1,15 @@
+package edu.mit.compilers.codegen.dataflow;
+
+import java.util.Collection;
+
+import edu.mit.compilers.codegen.DataFlowNode;
+
+public class DataFlowUtil {
+
+    private DataFlowUtil() {}
+
+    public static Collection<DataFlowNode> reachableFrom(DataFlowNode start) {
+        // TODO(jasonpr): Implement.
+        throw new RuntimeException("Not yet implemented.");
+    }
+}
