30ea9b09a006e81ebbf3136b964eb2c5926b696f
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-01 21:29:32 -0400

    Add interface for data flow optimization.

---
+++ src/edu/mit/compilers/codegen/dataflow/DataFlowOptimizer.java
@@ -0,0 +1,13 @@
+package edu.mit.compilers.codegen.dataflow;
+
+public interface DataFlowOptimizer {
+
+    /**
+     * Make an optimized data flow graph for the graph that starts at 'head'.
+     *
+     * <p>The optimization is performed in place.  That is, the data flow graph is mutated.
+     *
+     * @param head The head of the data flow graph to optimize.
+     */
+    public void optimize(DataFlowNode head);
+}
