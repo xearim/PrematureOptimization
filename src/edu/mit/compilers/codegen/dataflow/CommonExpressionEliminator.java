71a78704d641897a6785920838ea6db0080a406c
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-01 21:30:19 -0400

    Add skeleton CommonExpressionEliminator.

---
+++ src/edu/mit/compilers/codegen/dataflow/CommonExpressionEliminator.java
@@ -0,0 +1,18 @@
+package edu.mit.compilers.codegen.dataflow;
+
+/**
+ * Preforms CSE (common subexpression eliminator).
+ *
+ * <p>Currently, subexpressions aren't eliminated-- only full expressions are eliminated.
+ * That is, if (x + y) is available, we will not (yet!) optimize (1 + x + y).
+ *
+ * This should really be a non-instantiable class with a static optimize method, but
+ * it needs to be instantiable to conform to DataFlowOptimizer's interface.
+ */
+public class CommonExpressionEliminator implements DataFlowOptimizer {
+
+    @Override
+    public void optimize(DataFlowNode head) {
+        // TODO(jasonpr): Implement!
+    }
+}
