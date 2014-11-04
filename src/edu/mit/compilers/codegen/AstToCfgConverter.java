e9fdd3c976a8fed68d1782ed03d7303a4b061580
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-03 23:51:31 -0500

    Add Javadoc to AstToCfgConverter.

--- src/edu/mit/compilers/codegen/AstToCfgConverter.java
+++ src/edu/mit/compilers/codegen/AstToCfgConverter.java
@@ -6,7 +6,15 @@ import edu.mit.compilers.codegen.controllinker.MethodGraphFactory;
 import edu.mit.compilers.codegen.dataflow.BlockDataFlowFactory;
 import edu.mit.compilers.codegen.dataflow.DataFlow;
 
+/**
+ * Converts AST method nodes to control flow graphs.
+ *
+ * Eventually, this will be the place where we optionally apply optimizations.
+ */
 public class AstToCfgConverter {
+
+    private AstToCfgConverter() {}
+
     public static BiTerminalGraph convert(Method method) {
         DataFlow dataFlowGraph = new BlockDataFlowFactory(method.getBlock()).getDataFlow();
         return new MethodGraphFactory(dataFlowGraph, method.getName(),
