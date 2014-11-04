8a994731476e33e5f40eac2406ee2640fa9b99ac
Manuel Cabral <cabral@mit.edu>
2014-11-04 05:08:28 -0500

    Added hashCode and equals to ScopedVariable.

--- src/edu/mit/compilers/optimization/ScopedVariable.java
+++ src/edu/mit/compilers/optimization/ScopedVariable.java
@@ -37,4 +37,36 @@ public class ScopedVariable {
         throw new UnsupportedOperationException("Variable#getVariablesOf unimplemented");
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((location == null) ? 0 : location.hashCode());
+        result = prime * result + ((scope == null) ? 0 : scope.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ScopedVariable other = (ScopedVariable) obj;
+        if (location == null) {
+            if (other.location != null)
+                return false;
+        } else if (!location.equals(other.location))
+            return false;
+        if (scope == null) {
+            if (other.scope != null)
+                return false;
+        } else if (!scope.equals(other.scope))
+            return false;
+        return true;
+    }
+
 }
