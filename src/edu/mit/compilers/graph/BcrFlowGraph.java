8eb9e6e55d3dcce4fdc71cccac28bc288bf91ca5
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-18 15:29:06 -0500

    Update BcrFlowGraph.Builder.

--- src/edu/mit/compilers/graph/BcrFlowGraph.java
+++ src/edu/mit/compilers/graph/BcrFlowGraph.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 
 import edu.mit.compilers.codegen.asm.instructions.JumpType;
+import edu.mit.compilers.graph.BasicFlowGraph.Builder;
 
 public class BcrFlowGraph<T> implements FlowGraph<T> {
 
@@ -14,7 +15,7 @@ public class BcrFlowGraph<T> implements FlowGraph<T> {
     private final Node<T> continueTerminal;
     private final Node<T> returnTerminal;
 
-    public BcrFlowGraph(FlowGraph<T> flowGraph,
+    private BcrFlowGraph(FlowGraph<T> flowGraph,
             Node<T> breakTerminal, Node<T> continueTerminal, Node<T> returnTerminal) {
         this.flowGraph = flowGraph;
         this.breakTerminal = breakTerminal;
@@ -79,5 +80,78 @@ public class BcrFlowGraph<T> implements FlowGraph<T> {
 
     public Node<T> getReturnTerminal() {
         return returnTerminal;
+    }
+
+    public static <T> Builder<T> builder() {
+        return new Builder<T>();
+    }
+
+    public static class Builder<T> {
+        private Node<T> breakTerminal = Node.nop();
+        private Node<T> continueTerminal = Node.nop();
+        private Node<T> returnTerminal = Node.nop();
+        private final BasicFlowGraph.Builder<T> basicBuilder = BasicFlowGraph.builder();
+
+        public Node<T> getBreakTerminal() {
+            return breakTerminal;
+        }
+
+        public Builder<T> setBreakTerminal(Node<T> breakTerminal) {
+            this.breakTerminal = breakTerminal;
+            return this;
+        }
+
+        public Node<T> getContinueTerminal() {
+            return continueTerminal;
+        }
+
+        public Builder<T> setContinueTerminal(Node<T> continueTerminal) {
+            this.continueTerminal = continueTerminal;
+            return this;
+        }
+
+        public Node<T> getReturnTerminal() {
+            return returnTerminal;
+        }
+
+        public Builder<T> setReturnTerminal(Node<T> returnTerminal) {
+            this.returnTerminal = returnTerminal;
+            return this;
+        }
+
+        public Builder<T> link(Node<T> source, Node<T> sink) {
+            basicBuilder.link(source, sink);
+            return this;
+        }
+        
+        public Builder<T> linkNonJumpBranch(Node<T> branchPoint, Node<T> nonJumpBranch) {
+            basicBuilder.linkNonJumpBranch(branchPoint, nonJumpBranch);
+            return this;
+        }
+        
+        public Builder<T> linkJumpBranch(Node<T> branchPoint, JumpType type, Node<T> jumpBranch) {
+            basicBuilder.linkJumpBranch(branchPoint, type, jumpBranch);
+            return this;
+        }
+
+        public Builder<T> append(Node<T> node) {
+            basicBuilder.append(node);
+            return this;
+        }
+        
+        public Builder<T> append(T value) {
+            basicBuilder.append(value);
+            return this;
+        }
+        
+        public Builder<T> setEndToSinkFor(Node<T> node, Node<T> otherNode) {
+            basicBuilder.setEndToSinkFor(node, otherNode);
+            return this;
+        }
+
+        public BcrFlowGraph<T> build() {
+            return new BcrFlowGraph<T>(
+                    basicBuilder.build(), breakTerminal, continueTerminal, returnTerminal);
+        }
     }
 }
