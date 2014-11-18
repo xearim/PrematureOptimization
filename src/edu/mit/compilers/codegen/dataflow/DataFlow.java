da65b690ee2f83ac935abce5e0426f1e3ba69c70
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-18 16:54:04 -0500

    Allow DataFlow to convert itself to a FlowGraph.

--- src/edu/mit/compilers/codegen/dataflow/DataFlow.java
+++ src/edu/mit/compilers/codegen/dataflow/DataFlow.java
@@ -1,9 +1,33 @@
 package edu.mit.compilers.codegen.dataflow;
 
-import com.google.common.base.Preconditions;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Iterables;
+
+import edu.mit.compilers.ast.Condition;
+import edu.mit.compilers.codegen.AssignmentDataFlowNode;
 import edu.mit.compilers.codegen.BranchSinkDataFlowNode;
+import edu.mit.compilers.codegen.BranchSourceDataFlowNode;
+import edu.mit.compilers.codegen.BranchingControlFlowNode;
+import edu.mit.compilers.codegen.CompareDataFlowNode;
+import edu.mit.compilers.codegen.ControlFlowNode;
+import edu.mit.compilers.codegen.DataFlowNode;
+import edu.mit.compilers.codegen.MethodCallDataFlowNode;
+import edu.mit.compilers.codegen.NopDataFlowNode;
+import edu.mit.compilers.codegen.ReturnStatementDataFlowNode;
+import edu.mit.compilers.codegen.SequentialControlFlowNode;
 import edu.mit.compilers.codegen.SequentialDataFlowNode;
+import edu.mit.compilers.codegen.asm.instructions.Instruction;
+import edu.mit.compilers.graph.BasicFlowGraph;
+import edu.mit.compilers.graph.BcrFlowGraph;
+import edu.mit.compilers.graph.FlowGraph;
+import edu.mit.compilers.graph.Node;
 
 /**
  * Mutable representation of a data flow graph.
@@ -144,5 +168,110 @@ public class DataFlow {
 		return new DataFlow(nodes[0], end,
 				new DataControlNodes(breakNode, continueNode, returnNode));
 	}
-	
+    
+    /**
+     * Get the BcrFlowGraph representation of this DataFlow.
+     *
+     * <p>DataFlows are now deprecated in favor of FlowGraph<ScopedStatement>.  This
+     * method exists to help us transition.
+     */
+    public FlowGraph<ScopedStatement> asDataFlowGraph() {
+        BcrFlowGraph.Builder<ScopedStatement> builder = BcrFlowGraph.builder();
+        
+        Map<DataFlowNode, Node<ScopedStatement>> newNodes =
+                new HashMap<DataFlowNode, Node<ScopedStatement>>();
+        // The beginning node must be added upfront, because it has no parent.  (Other nodes
+        // are added when their parent is processed.)
+        newNodes.put(beginning, newNode(beginning));
+        // Add these other nodes upfront, too, so we're guaranteed to have a new node for
+        // each terminal, even if it's unreachable.
+        newNodes.put(end, newNode(end));
+        newNodes.put(controlNodes.breakNode, newNode(controlNodes.breakNode));
+        newNodes.put(controlNodes.continueNode, newNode(controlNodes.continueNode));
+        newNodes.put(controlNodes.returnNode, newNode(controlNodes.returnNode));
+
+        Set<DataFlowNode> visited = new HashSet<DataFlowNode>();
+        Deque<DataFlowNode> agenda = new ArrayDeque<DataFlowNode>();
+        
+        agenda.push(beginning);
+        while(!agenda.isEmpty()) {
+            DataFlowNode node = agenda.pop();
+            if (visited.contains(node)) {
+                continue;
+            }
+            visited.add(node);
+            
+            if (node instanceof BranchSinkDataFlowNode
+                    || node instanceof SequentialDataFlowNode) {
+                Set<DataFlowNode> successors = node.getSuccessors();
+                
+                if (!successors.isEmpty()) {
+                    // Only BranchSourceDataFlowNode has multiple successors.
+                    DataFlowNode next = Iterables.getOnlyElement(successors);
+                    agenda.push(next);
+
+                    if (!newNodes.containsKey(next)) {
+                        newNodes.put(next, newNode(next));
+                    }
+                    builder.link(newNodes.get(node), newNodes.get(next));
+                }
+            } else if (node instanceof BranchSourceDataFlowNode) {
+                BranchSourceDataFlowNode sourceNode = (BranchSourceDataFlowNode) node;
+                
+                DataFlowNode nonJumpBranch = sourceNode.getTrueBranch();
+                agenda.push(nonJumpBranch);
+                if (!newNodes.containsKey(nonJumpBranch)) {
+                    newNodes.put(nonJumpBranch, newNode(nonJumpBranch));
+                }
+                builder.linkNonJumpBranch(newNodes.get(node), newNodes.get(nonJumpBranch));
+                
+                DataFlowNode jumpBranch = sourceNode.getFalseBranch();
+                agenda.push(jumpBranch);
+                if (!newNodes.containsKey(jumpBranch)) {
+                    newNodes.put(jumpBranch, newNode(jumpBranch));
+                }
+                builder.linkJumpBranch(
+                        newNodes.get(node), sourceNode.getType(), newNodes.get(nonJumpBranch));
+            } else {
+                throw new AssertionError("Unexpected node type for " + node);
+            }
+        }
+        builder.setStartTerminal(newNodes.get(beginning));
+        builder.setEndTerminal(newNodes.get(end));
+        builder.setBreakTerminal(newNodes.get(controlNodes.breakNode));
+        builder.setContinueTerminal(newNodes.get(controlNodes.continueNode));
+        builder.setReturnTerminal(newNodes.get(controlNodes.returnNode));
+        
+        return builder.build();
+    }
+
+    private Node<ScopedStatement> newNode(DataFlowNode node) {
+        if (node instanceof BranchSinkDataFlowNode
+                || node instanceof BranchSourceDataFlowNode
+                || node instanceof NopDataFlowNode) {
+            return Node.nop();
+        } else if (node instanceof AssignmentDataFlowNode) {
+            AssignmentDataFlowNode aNode = (AssignmentDataFlowNode) node;
+            return Node.of(new ScopedStatement(
+                    aNode.getAssignment(),
+                    aNode.getScope()));
+        } else if (node instanceof CompareDataFlowNode) {
+            CompareDataFlowNode cNode = (CompareDataFlowNode) node;
+            return Node.of(new ScopedStatement(
+                    new Condition(cNode.getExpression().get()),
+                    cNode.getScope()));
+        } else if (node instanceof MethodCallDataFlowNode) {
+            MethodCallDataFlowNode mNode = (MethodCallDataFlowNode) node;
+            return Node.of(new ScopedStatement(
+                    mNode.getMethodCall(),
+                    mNode.getScope()));
+        } else if (node instanceof ReturnStatementDataFlowNode) {
+            ReturnStatementDataFlowNode rNode = (ReturnStatementDataFlowNode) node;
+            return Node.of(new ScopedStatement(
+                    rNode.getReturnStatement(),
+                    rNode.getScope()));
+        } else {
+            throw new AssertionError("Unexpected node type for " + node);
+        }
+    }
 }
