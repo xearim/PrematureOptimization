39d93d851ccfc68d6113bab429f926cc862704aa
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-18 19:12:34 -0500

    Make MethodGraphFactory and ErrorExitGraphFactory use FlowGraphs.
    This change breaks compilation even further.  It will be fixed.

--- src/edu/mit/compilers/codegen/controllinker/ErrorExitGraphFactory.java
+++ src/edu/mit/compilers/codegen/controllinker/ErrorExitGraphFactory.java
@@ -4,58 +4,41 @@ import static edu.mit.compilers.codegen.asm.instructions.Instructions.compareFla
 import static edu.mit.compilers.codegen.asm.instructions.Instructions.move;
 import static edu.mit.compilers.codegen.asm.instructions.Instructions.pop;
 import static edu.mit.compilers.codegen.asm.instructions.Instructions.ret;
-import edu.mit.compilers.codegen.BranchingControlFlowNode;
 import edu.mit.compilers.codegen.asm.Architecture;
 import edu.mit.compilers.codegen.asm.Literal;
 import edu.mit.compilers.codegen.asm.Location;
 import edu.mit.compilers.codegen.asm.Register;
+import edu.mit.compilers.codegen.asm.instructions.Instruction;
 import edu.mit.compilers.codegen.asm.instructions.JumpType;
+import edu.mit.compilers.graph.BasicFlowGraph;
+import edu.mit.compilers.graph.FlowGraph;
+import edu.mit.compilers.graph.Node;
 
-public class ErrorExitGraphFactory implements GraphFactory {
-    private final BiTerminalGraph graph;
+public class ErrorExitGraphFactory {
+    private final Literal exitValue;
 
     public ErrorExitGraphFactory(Literal exitValue) {
-        this.graph = constructGraph(exitValue);
+        this.exitValue = exitValue;
     }
 
-    private BiTerminalGraph constructGraph(Literal exitValue) {
-    	
-    	// Set R10 to the global for the intial RBP
-    	BiTerminalGraph initialize = BiTerminalGraph.ofInstructions(
-    			move(Architecture.MAIN_BASE_POINTER_ERROR_VARIABLE, Register.R10)
-    			);
-    	
-    	// Check if the current RBP is the initial RBP
-    	BiTerminalGraph compareBasePointer = BiTerminalGraph.ofInstructions(
-    			compareFlagged(Register.RBP, Register.R10));
-    	
-    	// If it isnt, we are going to recurse into the old RBP
-    	BiTerminalGraph cleanMethodScope = BiTerminalGraph.ofInstructions(
-    			move(new Location(Register.RBP, 0*Architecture.BYTES_PER_ENTRY), Register.RBP));
-    	
-    	// When we exit, we need to restore RBP and RSP and then put the error code into RAX
-    	BiTerminalGraph exit = BiTerminalGraph.ofInstructions(
-    			move(Register.RBP, Register.RSP),
-    			pop(Register.RBP),
-    			move(exitValue, Register.RAX),
-    			ret());
-    	
-    	// We branch between recursively setting RBP and just exiting with our error code
-    	BranchingControlFlowNode branch = new BranchingControlFlowNode(
-    			JumpType.JE,
-    			cleanMethodScope.getBeginning(),
-    			exit.getBeginning());
-    	
-    	// Link all the graph nodes up
-    	initialize.getEnd().setNext(compareBasePointer.getBeginning());
-    	compareBasePointer.getEnd().setNext(branch);
-    	cleanMethodScope.getEnd().setNext(compareBasePointer.getBeginning());
-
-        return new BiTerminalGraph(initialize.getBeginning(), exit.getEnd());
-    }
-
-    @Override
-    public BiTerminalGraph getGraph() {
-        return graph;
+    public FlowGraph<Instruction> getGraph() {
+        // We need to be able to identify these nodes for the loop.
+        Node<Instruction> compareBasePointer = Node.of(compareFlagged(Register.RBP, Register.R10));
+        Node<Instruction> cleanMethodScope = Node.of(
+                move(new Location(Register.RBP, 0*Architecture.BYTES_PER_ENTRY), Register.RBP));
+        Node<Instruction> exitNop = Node.nop();
+        
+        BasicFlowGraph.Builder<Instruction> builder = BasicFlowGraph.builder();
+        return builder.append(move(Architecture.MAIN_BASE_POINTER_ERROR_VARIABLE, Register.R10))
+                .append(compareBasePointer)
+                .linkNonJumpBranch(compareBasePointer, cleanMethodScope)
+                .linkJumpBranch(compareBasePointer, JumpType.JE, exitNop)
+                .link(cleanMethodScope, compareBasePointer)  // Connect the back edge.
+                .setEnd(exitNop)
+                .append(move(Register.RBP, Register.RSP))
+                .append(pop(Register.RBP))
+                .append(move(exitValue, Register.RAX))
+                .append(ret())
+                .build();
     }
 }
--- src/edu/mit/compilers/codegen/controllinker/MethodGraphFactory.java
+++ src/edu/mit/compilers/codegen/controllinker/MethodGraphFactory.java
@@ -10,6 +10,8 @@ import edu.mit.compilers.codegen.asm.Literal;
 import edu.mit.compilers.codegen.asm.Register;
 import edu.mit.compilers.codegen.asm.instructions.Instruction;
 import edu.mit.compilers.codegen.dataflow.ScopedStatement;
+import edu.mit.compilers.graph.BasicFlowGraph;
+import edu.mit.compilers.graph.BcrFlowGraph;
 import edu.mit.compilers.graph.FlowGraph;
 
 /**
@@ -42,38 +44,33 @@ public class MethodGraphFactory {
             String name, boolean isVoid, long entriesToAllocate) {
         // If we are the main method, we need to write down the base pointer for error handling
         boolean isMain = name.equals(Architecture.MAIN_METHOD_NAME);
-        BiTerminalGraph enterInstruction = isMain
-                ? BiTerminalGraph.ofInstructions(
-                        enter(entriesToAllocate),
-                        move(Register.RBP, Architecture.MAIN_BASE_POINTER_ERROR_VARIABLE))
-                : BiTerminalGraph.ofInstructions(enter(entriesToAllocate));
+        
+        BasicFlowGraph.Builder<Instruction> builder = BasicFlowGraph.builder();
+        
+        // Entry code.
+        builder.append(enter(entriesToAllocate));
+        if (isMain){
+            builder.append(move(Register.RBP, Architecture.MAIN_BASE_POINTER_ERROR_VARIABLE));
+        }
+        
+        // Block graph.
+        BcrFlowGraph<Instruction> blockGraph =
+                new DataFlowGraphFactory(methodDataFlowGraph).getGraph(); 
+        builder.append(blockGraph);
 
-        //DataFlow test = new BlockDataFlowFactory(block).getDataFlow();				
-        ControlTerminalGraph blockGraph = new DataFlowGraphFactory(
-                methodDataFlowGraph).getGraph();
-        //ControlTerminalGraph blockGraph = new BlockGraphFactory(block).getGraph();
-        // Link the Entry instruction to the start of the block
-        enterInstruction.getEnd().setNext(blockGraph.getBeginning());
+        // Fall Through Checking.
+        if (!isVoid) {
+            builder.append(new ErrorExitGraphFactory(Literal.CONTROL_DROP_OFF_EXIT).getGraph());
+        }
 
-        BiTerminalGraph returnInstruction = BiTerminalGraph.ofInstructions(
-        										leave(),
-        										ret());
-        ControlFlowNode sink = returnInstruction.getBeginning();
-
-        BiTerminalGraph fallThroughChecker = isVoid
-                ? BiTerminalGraph.ofInstructions()
-                : new ErrorExitGraphFactory(Literal.CONTROL_DROP_OFF_EXIT).getGraph();
+        builder.setEndToSinkFor(builder.getEnd(), blockGraph.getReturnTerminal())
+                .append(leave())
+                .append(ret());
 
         // TODO(jasonpr): Connect break and continue to an explicit error thrower.
         // The semantic checker should ensure that we'll never have one in a method's block,
         // but... just in case.
-        blockGraph.getControlNodes().getReturnNode().setNext(sink);
-        blockGraph.getEnd().setNext(fallThroughChecker.getBeginning());
-        fallThroughChecker.getEnd().setNext(sink);
-
-        BiTerminalGraph result = new BiTerminalGraph(enterInstruction.getBeginning(), returnInstruction.getEnd());
-
-        return result.asFlowGraph();
+        return builder.build();
     }
 
     public FlowGraph<Instruction> getGraph() {
