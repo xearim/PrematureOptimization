8529a66c7397f7f0505073346c4ed511065dbfd5
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-19 22:13:53 -0500

    convert StringLiteralGraphFactory to a FlowGraph rep.

--- src/edu/mit/compilers/codegen/controllinker/StringLiteralGraphFactory.java
+++ src/edu/mit/compilers/codegen/controllinker/StringLiteralGraphFactory.java
@@ -4,24 +4,24 @@ import static edu.mit.compilers.codegen.asm.instructions.Instructions.push;
 import edu.mit.compilers.ast.StringLiteral;
 import edu.mit.compilers.codegen.asm.Label;
 import edu.mit.compilers.codegen.asm.Label.LabelType;
+import edu.mit.compilers.codegen.asm.instructions.Instruction;
 import edu.mit.compilers.common.Variable;
+import edu.mit.compilers.graph.BasicFlowGraph;
+import edu.mit.compilers.graph.FlowGraph;
 
 public class StringLiteralGraphFactory implements GraphFactory {
-    private final BiTerminalGraph graph;
-    private final Label stringLabel;
+    private final StringLiteral stringLiteral;
 
     public StringLiteralGraphFactory(StringLiteral stringLiteral) {
-    	this.stringLabel = new Label(LabelType.STRING, Variable.forCompiler(stringLiteral.getID()));
-        this.graph = calculateStringLiteral(stringLiteral);
-    }
-    
-    public BiTerminalGraph calculateStringLiteral(StringLiteral stringLiteral){
-    	return BiTerminalGraph.ofInstructions(
-                push(stringLabel));
+        this.stringLiteral = stringLiteral;
     }
 
-	@Override
-	public BiTerminalGraph getGraph() {
-		return graph;
-	}
+    @Override
+    public FlowGraph<Instruction> getGraph() {
+        Label stringLabel = new Label(LabelType.STRING,
+                Variable.forCompiler(stringLiteral.getID()));
+        return BasicFlowGraph.<Instruction>builder()
+                .append(push(stringLabel))
+                .build();
+    }
 }
