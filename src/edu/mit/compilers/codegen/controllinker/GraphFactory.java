e9b20cf21dab215e8896875e7002e8b90a6a9ce5
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-19 20:25:44 -0500

    Make Graph factory return a FlowGraph<Instruction>.
    This breaks even more code. It's a necessary part of our transition.

--- src/edu/mit/compilers/codegen/controllinker/GraphFactory.java
+++ src/edu/mit/compilers/codegen/controllinker/GraphFactory.java
@@ -1,10 +1,13 @@
 package edu.mit.compilers.codegen.controllinker;
 
+import edu.mit.compilers.codegen.asm.instructions.Instruction;
+import edu.mit.compilers.graph.FlowGraph;
+
 /**
  * Something that makes a BiTerminalGraph.
  * 
  * <p>Most implementations will take an AST node as a constructor argument.
  */
 public interface GraphFactory {
-    public BiTerminalGraph getGraph();
+    public FlowGraph<Instruction> getGraph();
 }
