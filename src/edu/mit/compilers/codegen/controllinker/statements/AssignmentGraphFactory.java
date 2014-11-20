8b02e2df808324e2c85420ed80cff784a922453f
Jason A Paller-Rzepka <jasonpr@mit.edu>
2014-11-19 22:22:07 -0500

    Convert AssignmentGraphFactory to a FlowGraph rep.

--- src/edu/mit/compilers/codegen/controllinker/statements/AssignmentGraphFactory.java
+++ src/edu/mit/compilers/codegen/controllinker/statements/AssignmentGraphFactory.java
@@ -14,11 +14,12 @@ import edu.mit.compilers.ast.NativeExpression;
 import edu.mit.compilers.ast.ScalarLocation;
 import edu.mit.compilers.ast.Scope;
 import edu.mit.compilers.codegen.asm.VariableReference;
-import edu.mit.compilers.codegen.controllinker.BiTerminalGraph;
+import edu.mit.compilers.codegen.asm.instructions.Instruction;
 import edu.mit.compilers.codegen.controllinker.GraphFactory;
 import edu.mit.compilers.codegen.controllinker.NativeExprGraphFactory;
 import edu.mit.compilers.codegen.controllinker.VariableLoadGraphFactory;
-import edu.mit.compilers.common.Variable;
+import edu.mit.compilers.graph.BasicFlowGraph;
+import edu.mit.compilers.graph.FlowGraph;
 
 public class AssignmentGraphFactory implements GraphFactory {
 
@@ -27,7 +28,7 @@ public class AssignmentGraphFactory implements GraphFactory {
     private final Scope scope;
     private final Location target;
     private final boolean check;
-    
+
     public AssignmentGraphFactory(Location target, AssignmentOperation op, NativeExpression expr, Scope scope, boolean check){
         this.target = target;
         this.op = op;
@@ -35,8 +36,8 @@ public class AssignmentGraphFactory implements GraphFactory {
         this.scope = scope;
         this.check = check;
     }
-    
-    private BiTerminalGraph calculateStore(Location target, Scope scope){
+
+    private FlowGraph<Instruction> calculateStore(Location target, Scope scope){
         if (target instanceof ArrayLocation) {
             return calculateStoreToArray((ArrayLocation) target, scope);
         } else if (target instanceof ScalarLocation) {
@@ -45,56 +46,47 @@ public class AssignmentGraphFactory implements GraphFactory {
             throw new AssertionError("Unexpected location type for " + target);
         }
     }
-    
-    private BiTerminalGraph calculateStoreToArray(ArrayLocation target, Scope scope){
+
+    private FlowGraph<Instruction> calculateStoreToArray(ArrayLocation target, Scope scope){
         // TODO(jasonpr): Have this code live somewhere sensible.
         return VariableLoadGraphFactory.calculateStoreToArray(target, scope, check);
     }
-    
-    private BiTerminalGraph calculateStoreToScalar(ScalarLocation target, Scope scope){
-        Variable variable = target.getVariable();
-        return BiTerminalGraph.ofInstructions(
-                pop(R11),
-                move(R11, new VariableReference(variable, scope))
-                );
-    }
-    
-    @Override
-    public BiTerminalGraph getGraph() {
-        switch(op){
-        case MINUS_EQUALS:
-            return BiTerminalGraph.sequenceOf(
-                    new VariableLoadGraphFactory(target, scope).getGraph(),
-                    new NativeExprGraphFactory(expr, scope).getGraph(),
-                    BiTerminalGraph.ofInstructions(
-                            pop(R10),
-                            pop(R11),
-                            subtract(R10, R11),
-                            push(R11)
-                            ),
-                    calculateStore(target, scope)
-                    );
-        case PLUS_EQUALS:
-            return BiTerminalGraph.sequenceOf(
-                    new VariableLoadGraphFactory(target, scope).getGraph(),
-                    new NativeExprGraphFactory(expr, scope).getGraph(),
-                    BiTerminalGraph.ofInstructions(
-                            pop(R10),
-                            pop(R11),
-                            add(R10, R11),
-                            push(R11)
-                            ),
-                    calculateStore(target, scope)
-                    );
-        case SET_EQUALS:
-            return BiTerminalGraph.sequenceOf(
-                        new NativeExprGraphFactory(expr, scope).getGraph(),
-                        calculateStore(target, scope)
-                        );
-        default:
-            throw new AssertionError("Unexpected operator: " + op.getSymbol());
-        
-        }
+
+    private FlowGraph<Instruction> calculateStoreToScalar(ScalarLocation target, Scope scope){
+        return BasicFlowGraph.<Instruction>builder()
+                .append(pop(R11))
+                .append(move(R11, new VariableReference(target.getVariable(), scope)))
+                .build();
     }
 
+    @Override
+    public FlowGraph<Instruction> getGraph() {
+        BasicFlowGraph.Builder<Instruction> builder = BasicFlowGraph.builder();
+        switch(op){
+        case MINUS_EQUALS:
+            return builder.append(new VariableLoadGraphFactory(target, scope).getGraph())
+                    .append(new NativeExprGraphFactory(expr, scope).getGraph())
+                    .append(pop(R10))
+                    .append(pop(R11))
+                    .append(subtract(R10, R11))
+                    .append(push(R11))
+                    .append(calculateStore(target, scope))
+                    .build();
+        case PLUS_EQUALS:
+            return builder.append(new VariableLoadGraphFactory(target, scope).getGraph())
+                    .append(new NativeExprGraphFactory(expr, scope).getGraph())
+                    .append(pop(R10))
+                    .append(pop(R11))
+                    .append(add(R10, R11))
+                    .append(push(R11))
+                    .append(calculateStore(target, scope))
+                    .build();
+        case SET_EQUALS:
+            return builder.append(new NativeExprGraphFactory(expr, scope).getGraph())
+                    .append(calculateStore(target, scope))
+                    .build();
+        default:
+            throw new AssertionError("Unexpected operator: " + op.getSymbol());
+        }
+    }
 }
