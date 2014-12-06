729ac18fce48c3c8cd47fee89066e76202a4fe73
Manuel Cabral <cabral@mit.edu>
2014-12-05 21:41:15 -0500

    Added skeleton for LivenessSpec

---
+++ src/edu/mit/compilers/optimization/LivenessSpec.java
@@ -0,0 +1,47 @@
+package edu.mit.compilers.optimization;
+
+import static edu.mit.compilers.common.SetOperators.union;
+
+import java.util.Collection;
+import java.util.Set;
+
+import edu.mit.compilers.codegen.dataflow.ScopedStatement;
+import edu.mit.compilers.common.Variable;
+import edu.mit.compilers.graph.Node;
+
+public class LivenessSpec implements AnalysisSpec<ScopedStatement, Variable> {
+
+    /** Liveness algorithm propagates backwards */
+    public boolean isForward() {
+        return false;
+    }
+
+    @Override
+    public Set<Variable> getGenSet(Node<ScopedStatement> node) {
+        throw new UnsupportedOperationException("unimplemented");
+    }
+
+    @Override
+    public boolean mustKill(Node<ScopedStatement> currentNode,
+            Variable candidate) {
+        throw new UnsupportedOperationException("unimplemented");
+    }
+
+    @Override
+    public Set<Variable> applyConfluenceOperator(
+            Iterable<Collection<Variable>> inputs) {
+        return union(inputs);
+    }
+
+    @Override
+    public boolean gensImmuneToKills() {
+        return true;
+    }
+
+    @Override
+    public Set<Node<ScopedStatement>> filterNodes(
+            Iterable<Node<ScopedStatement>> nodes) {
+        throw new UnsupportedOperationException("unimplemented");
+    }
+
+}
