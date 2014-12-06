f91ba1600ca26189b8236446babf0203bfd25bac
Manuel Cabral <cabral@mit.edu>
2014-12-05 22:40:37 -0500

    Moved ReachingDefSpec#redefined to Util

--- src/edu/mit/compilers/optimization/Util.java
+++ src/edu/mit/compilers/optimization/Util.java
@@ -4,12 +4,14 @@ import java.util.Set;
 
 import com.google.common.collect.ImmutableSet;
 
+import edu.mit.compilers.ast.Assignment;
 import edu.mit.compilers.ast.BinaryOperation;
 import edu.mit.compilers.ast.FieldDescriptor;
 import edu.mit.compilers.ast.GeneralExpression;
 import edu.mit.compilers.ast.Location;
 import edu.mit.compilers.ast.MethodCall;
 import edu.mit.compilers.ast.Scope;
+import edu.mit.compilers.ast.StaticStatement;
 import edu.mit.compilers.ast.TernaryOperation;
 import edu.mit.compilers.ast.UnaryOperation;
 import edu.mit.compilers.codegen.dataflow.ScopedStatement;
@@ -74,5 +76,26 @@ public class Util {
         }
 
         return builder.build();
+    }
+
+    /** Get the set of variables that are potentially redefined at a node. */
+    public static Set<ScopedVariable> redefined(Node<ScopedStatement> node) {
+        ImmutableSet.Builder<ScopedVariable> redefinedBuilder = ImmutableSet.builder();
+
+        ScopedStatement scopedStatement = node.value();
+        StaticStatement statement = scopedStatement.getStatement();
+        Scope scope = scopedStatement.getScope();
+        // Add LHS.
+        if (statement instanceof Assignment) {
+            ScopedVariable lhs = ScopedVariable.getAssigned((Assignment) statement, scope);
+            redefinedBuilder.add(lhs);
+        }
+
+        if (Util.containsMethodCall(statement.getExpression())) {
+            // For now, just assume that functions can redefine every global!
+            // TODO(jasonpr): Only add each function's global write set.
+            redefinedBuilder.addAll(Util.getGlobalVariables(scope));
+        }
+        return redefinedBuilder.build();
     }
 }
